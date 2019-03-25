1aa3a159eb5f5d2858d679fb4270207b1d12af67
Sameer Ali <32299374+TheSameerAli@users.noreply.github.com>
2019-03-25 13:21:18 +0000

    feat(data-model): Introduces data model into the system (#33)
    [GitHub]
    Issue #26

---
+++ src/frontend/src/app/models/media-file.ts
@@ -0,0 +1,13 @@
+import { Category } from './category';
+import { Playlist } from './playlist';
+import { Image } from './image';
+
+export class MediaFile {
+    name: string;
+    path: string;
+    type: string;
+    comment?: string;
+    categories?: Category[];
+    playlists?: Playlist[];
+    image: Image;
+}
